7e390c058543a58f094cf0f80d59594caa576ad0
Yavuz Selim KAHRAMAN <info@yavuzselimkahraman.com.tr>
2026-02-06 00:18:47 +0300

    logicaloperatorscompleted

--- logicaloperators.js
+++ logicaloperators.js
@@ -1,6 +1,6 @@
-// OR AND ve NOT 
+// OR AND ve NOT
 
-// operand boolean 
+// operand boolean
 
 // let value = 3;
 
@@ -9,9 +9,9 @@
 // console.log(result);
 
 // 2 true  => true
-// false false => false 
+// false false => false
 
-// veya operatörü true  true 
+// veya operatörü true  true
 
 // console.log ("true || true",true || true)
 // console.log("true || false",true || false)
@@ -24,33 +24,60 @@
 
 // let value = 3>5 || 3<2 || "önemsiz";
 
-
 // 3>5 || (value ="yavuz")
-
-
 
 // console.log("value",value)
 
-// 
+//
 
-console.log("true && true", true&& true);
-console.log("false && false", false && false);
-console.log("false && true", false && true);
-console.log("true && false", true && false);
+// console.log("true && true", true&& true);
+// console.log("false && false", false && false);
+// console.log("false && true", false && true);
+// console.log("true && false", true && false);
 
-if (3<5 && 3<2){
-console.log("çalışmak")
-}
+// if (3<5 && 3<2){
+// console.log("çalışmak")
+// }
 
-let control = 3>5 || 3>4 || "yavuz";
-console.log("control",control)
- if(control){
-   console.log("if çalıştı")
- }
+// let control = 3>5 || 3>4 || "yavuz";
+// console.log("control",control)
+//  if(control){
+//    console.log("if çalıştı")
+//  }
 
- if("1"){
+//  if("1"){
 
- }
- if("yavuz "){
+//  }
+//  if("yavuz "){
 
- }
+//  }
+
+// let orExample = 3>5 || 3>4 || "yavuz" || "selim";
+// console.log("orExample", orExample)
+
+// let andExample = 3>2 && "yavuz" && 0 && ""
+// console.log("andExample", andExample)
+
+//react.js
+// JSX <p> </p>
+
+// a == 3 && (<p> Merhaba Dünya</p>) W
+
+// let value = 3;
+// let returnValue = 3 == 3 && (value = 5) && (value = value +2);
+
+
+// let returnValue2 = 3 && 5 && "yavuz"
+
+// let returnValue3 = 3<2 || 5>8 || ""
+
+// console.log("returnValu3",returnValue3)
+// console.log("returnValue2",returnValue2)
+
+// console.log("value", value);
+// console.log("returnValue", returnValue);
+ 
+ let number = 5; 
+ number = !!number;
+
+ console.log("number ",number)
