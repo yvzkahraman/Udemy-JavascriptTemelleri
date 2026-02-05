1dc60eec44dab7b4c5e69c3963bb4890c493cd1c
Yavuz Selim KAHRAMAN <info@yavuzselimkahraman.com.tr>
2026-02-06 01:24:20 +0300

    while

---
+++ loops.js
@@ -0,0 +1,46 @@
+//döngü dönmek
+
+
+// console 3 tnae mer
+
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+// console.log("Merhaba dünya")
+
+
+//döngüler while | do while | for 
+// for 
+
+/*
+
+while(koşul){
+ ... 
+}
+
+// sonsuz döngü
+while(1){
+.....
+if()
+}
+if()
+*/
+
+let i = 150;
+while(i){
+    console.log("Merhaba Dünya")
+    // i=i+1;
+    // i+=1;
+    i--;
+}
