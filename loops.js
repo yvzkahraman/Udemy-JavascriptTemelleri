24ee1006d12298a16909447aaa163769c29aa87b
Yavuz Selim KAHRAMAN <info@yavuzselimkahraman.com.tr>
2026-02-06 18:45:33 +0300

    loops-completed

--- loops.js
+++ loops.js
@@ -52,9 +52,50 @@ do
 while(kosul)
 */
 
-let i =0;
-do{
+// let i =0;
+// do{
 
-    console.log("Merhaba dünya")
-    i++;
-}while(i<150)
+//     console.log("Merhaba dünya")
+//     i++;
+// }while(i<150)
+
+/* 
+for(tanimlama; kosul; artirim/azaltim){ 
+
+}
+
+for(let i = 0; i<5; i++){
+
+}
+
+*/
+
+// for (let i = 0; i < 5; i++) {
+//   if(i==3)
+//     break;
+//   console.log("Merhaba dünya for");
+// }
+
+// for (let i = 5; i; i--) {
+//     if(i == 3)
+//         continue;
+//   console.log("Merhaba dünya for azaltim");
+// }
+
+
+//  3   1 2 3
+//  1 => 1 2 3 2 => 1 2 3 3=> 1 2 3
+//etiket 
+
+label : for(let i =0 ; i<3; i++){
+    console.log("i",i)
+    console.log("------------------------")
+    for(let j = 0; j<3 ; j++){
+        if(j==1){
+            break label;
+        }
+        console.log("j",j)
+        
+    }
+    console.log("-----------------------")
+}
