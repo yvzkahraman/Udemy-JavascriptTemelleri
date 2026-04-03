30f6e2b5a4f0cad75ce3b18e6b86590f61ff716d
Yavuz Selim KAHRAMAN <info@yavuzselimkahraman.com.tr>
2026-04-04 02:12:03 +0300

    funcstions

---
+++ functions.js
@@ -0,0 +1,50 @@
+// iplik  => fabrika ... => gömlek, pantolon
+// süreçleri fonk
+
+// alert(), console.log(), prompt(), confirm()
+
+//  let returnValue = confirm()
+
+// let returnValue = confirm("Emin misiniz ?");
+// console.log("rv", returnValue);
+
+// //confirm() a=b alert("") if(cevap )
+
+// confirm()
+
+//void
+
+//let ile tanımlama yapıyorsan
+
+// let message= ""
+
+// function showMessage() {
+// //   let message = "mesaj";
+
+// // et yavuz = 34;l
+// message = "yavuz "
+
+//   console.log("function'ın içinden", message);
+//   // return "Mesaj gösterildi";
+// }
+
+// // console.log("yavuz",yavuz)
+
+// //  let returnValue = showMessage();
+
+// //  console.log("rv",returnValue);
+
+// showMessage();
+
+// iplik
+
+function showMessage(message) {
+  console.log("message", message);
+
+  message = message || "bilinmeyen mesaj";
+
+  alert("show message : " + message);
+}
+
+showMessage();
+
