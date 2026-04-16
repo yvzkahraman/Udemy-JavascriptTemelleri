8b2721fbe98afa064d62b216473485390a1dea8d
Yavuz Selim KAHRAMAN <info@yavuzselimkahraman.com.tr>
2026-04-17 01:12:01 +0300

    delete-csharp-projfe

---
+++ datatypes/arrays.js
@@ -0,0 +1,6 @@
+// let names = ["selim", "yavuz", "eymen", "zeynep"];
+// let names2 = ["ahmet"];
+
+// console.log("concat", names.concat(names2, "ayşe"));
+
+// console.log("names", names);
--- datatypes/strings.js
+++ datatypes/strings.js
@@ -37,3 +37,4 @@ console.log("trim", sentence.trim())
 
 
 console.log("replace", sentence.replace("Yavuz", "Selim"))
+h
